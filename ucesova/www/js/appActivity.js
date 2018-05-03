e3db94bb239e7a3100052f5d996fa5a5e151e67f
ucesova <ucesova@ucl.ac.uk>
2018-05-03 15:27:53 -0400

    going back

--- ucesova/www/js/appActivity.js
+++ ucesova/www/js/appActivity.js
@@ -112,23 +112,15 @@ function loadPOIlayer(POIdata) {
 }
 
 // code to track the user location
-var userPositions = []; // this array will store the user location each time they are watched
 var position_marker
 			
 function trackLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.watchPosition(showPosition);
 		navigator.geolocation.getCurrentPosition(getDistanceFromPoint);
-		userPositions.push({lat: position.coords.latitude, lng: position.coords.longitude}); // Save the current position to the array. based on https://stackoverflow.com/questions/47752531/saving-user-input-location-from-current-location-google-maps-api and https://stackoverflow.com/questions/15742442/declaring-array-of-objects
-		console.log(userPositions);
-		/* for(var i = 0; i < userPositions.length; i++) {
-			if (userPositions[1] !== undefined && userPositions[i] !== userPositions[i+1]){
-				return trackLocation();
-			}
-		}  */
-	}else {
-		document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
-	}
+		} else {
+			document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser.";
+		}
 }
 		
 function showPosition(position) {
